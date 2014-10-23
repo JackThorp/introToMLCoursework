65ed216fd3476d853774f7f06e8b927faba64d4d
Jack Thorp <jackthorp13@gmail.com>
2014-10-23 18:51:17 +0100

    Basic function structure done. Added function to generate binary targets given an emotion. Main loop to create decision tree needs writing.

--- getDecisionTree.m
+++ getDecisionTree.m
@@ -1,21 +1,20 @@
-function [ decision_tree ] = getDecisionTree(emotion_number)
+function [ decision_tree ] = getDecisionTree(emotion)
 % Returns a decision tree for the given emotion number.
 %   The function generates the binary target for the emotion from the
 %   clean data set and then calls the decisionTreeLearning function to
 %   generate the decision tree.
-
-% load('/homes/jt1712/3rdYEar/ML/forstudents/cleandata_students.mat')
-
-% decision_tree = decisionTreeLearning(examples, attributes, binary_targets); 
-    newy = [1;2;3;4;5;6;1]
-    decision_tree = get_bin_tars(newy,1)
-
+    
+    load('forstudents/cleandata_students.mat');
+    binary_targets = get_bin_tars(y, emotion);
+    decision_tree = decisionTreeLearning(x, attributes, binary_targets); 
+    
 end
 
 function bin_tars = get_bin_tars(y, emotion)
-   
+% creates the binary targets vector for the given emotion number from the 
+% classifications given in y.
+
     bin_tars = zeros([length(y) 1])
-% iterrate through the array
     for i = 1:length(y)
         if y(i)==emotion
             bin_tars(i)=1;
