189f8299a02d2b6b3e75b2e38c38bd2268e7d9b3
Joel Auterson <joel.auterson@googlemail.com>
2014-10-28 16:35:50 +0000

    Slight refactor on getDecisionTree

--- getDecisionTree.m
+++ getDecisionTree.m
@@ -4,18 +4,10 @@ function [ decision_tree ] = getDecisionTree(emotion)
 %   clean data set and then calls the decisionTreeLearning function to
 %   generate the decision tree.
     load('forstudents/cleandata_students.mat');
-    binary_targets = get_bin_tars(y, emotion);
-    % TODO: define attributes, it is not the same as y
+    binary_targets = y;
+    for i=1:length(binary_targets),
+        binary_targets(i) = (binary_targets(i) == emotion);
+    end
     attributes = (1:length(x(1,:)));
     decision_tree = decisionTreeLearning(x, attributes, binary_targets); 
-end
-
-function [bin_tars] = get_bin_tars(y, emotion)
-% creates the binary targets vector for the given emotion number from the 
-% classifications given in y.
-
-    bin_tars = zeros([length(y) 1]);
-    for i = 1:length(y)
-        bin_tars(i) = (y(i) == emotion);
-    end
 end
