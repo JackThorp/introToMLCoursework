dd4c2a1f7127dcde0ef575a49a769e5df6ae2e18
Haiming Zhao <hmz11@imperial.ac.uk>
2014-10-30 18:27:56 +0000

    Added CV script. Error in testExample, testTree

---
+++ crossval_script.m
@@ -0,0 +1,21 @@
+data = load('forstudents/cleandata_students.mat');
+
+% for i = 1:10
+i=1;
+
+    [ training, validation ] = crossValidationSplit(10, data, i);
+    
+    %trees
+    decision_trees= cell(1,6);
+    for emotion = 1:6 
+        decision_trees{emotion} = getDecisionTree(emotion,training);
+    end
+    
+    % Get predictions of our trees for validation test data.
+    predictions = testTrees(decision_trees, validation.x);
+    
+    % Get confusion matrix from predictions and actual validation target
+    % emotions.
+    [C,order] = confusionmat(validation.y, predictions);
+    
+% end
