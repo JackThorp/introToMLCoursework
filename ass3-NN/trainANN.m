c71f09e80a21e7bde839bda6ff208ce4ada6b79f
Jack Thorp <jackthorp13@gmail.com>
2014-11-11 15:08:00 +0000

    Splitting the test correctly and coded in static validation splits for network training.

--- ass3-NN/trainANN.m
+++ ass3-NN/trainANN.m
@@ -1,27 +1,32 @@
 
-load('/homes/jt1712/3rdYear/ML/introToMLCoursework/forstudents/cleandata_students.mat')
+data = load('/homes/jt1712/3rdYear/ML/introToMLCoursework/forstudents/cleandata_students.mat');
 
-% prepare data (format by transposing) for neural network functions.
-[x2, y2] = ANNdata(x,y);
+% prepare data (format by transposing) for neural network functions
+
+[trainval, test] = crossValidationSplit(10, data, 1);
+[x2, y2] = ANNdata(trainval.x, trainval.y);
 
 % Create blank feedforward network
-net = feedforwardnet(5, 'traingd');
+net = feedforwardnet([10 5]);
 net = configure(net, x2, y2); %sets up network and initialises weights. Also configure is done automatically by train function.
 
 
 % configure and run training algorithm.
 % Stop criteria for training includes end of epochs, max_fail, min_grad,
 % time (timeout) or goal (minimum performance value).
-% net.trainParam.epochs   - sets number of epochs default = ...?
-% net.trainParam.lr       - sets learning rate, defualt value is 0.01
-% net.trainParam.max_fail - sets the number of validation checks allowed
-%                           before failure. Default is 6. i.e after 6 
-%                           increments of error function training stops. 
-% net.trainParam.min_grad - min gradient value that training will stop at,
-%                           default = 1e-5.
+% net.trainParam.min_grad
+net.trainParam.epochs = 1000;
+net.trainParam.lr = 0.01;
+net.trainParam.max_fail = 6;
+net.divideFcn = 'divideind';
+net.divideParam.valInd = 1:100;
+net.divideParam.trainInd = 101:900;
 
 % tr is the training record. 
 [net, tr] = train(net, x2, y2);
+
+% To retrain a network net = init(net); net = train(net, x2, y2); maybe
+% create a new type of feedforward net. 
 
 %Y = sim(net, y2);
 %plot(x2,y2,x2,Y, 'r.');
