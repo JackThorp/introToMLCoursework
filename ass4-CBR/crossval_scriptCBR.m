e38d01a1955e2ba2b0834327a932ea5f5b06843a
Haiming Zhao <hai.zhao11@imperial.ac.uk>
2014-11-29 00:34:53 +0000

    fix crossval script

--- ass4-CBR/crossval_scriptCBR.m
+++ ass4-CBR/crossval_scriptCBR.m
@@ -29,19 +29,20 @@ c_matrix = sum(reshape(cell2mat(matrices), [ size(matrices{1}), length(matrices)
 fprintf('The combined matrix is:\n');
 disp(c_matrix);
 
-measures = struct('class', {1,2,3,4,5,6});
+measures = struct('class', {emolab2str(1),emolab2str(2),emolab2str(3),...
+    emolab2str(4),emolab2str(5),emolab2str(6)});
 
 % measures for each class
 for class=1:6
     recall = measure_recall(c_matrix, class);
-    measures.class(i).recall = recall;
+    measures(class).recall = recall;
     
     precision = measure_precision( c_matrix, class);
-    measures.class(i).precision = precision;
+    measures(class).precision = precision;
     
     F1 = 2 *( (precision*recall)/(precision+recall) );
-    measures.class(i).f1 = F1;
+    measures(class).f1 = F1;
 end
 
 cr = measure_cr(c_matrix);
-measures.cr = cr;
+measures(1).cr = cr;
